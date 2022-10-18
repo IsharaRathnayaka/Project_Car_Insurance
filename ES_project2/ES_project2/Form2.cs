d0c70f211f40ee6e71e73f0483efb45e71c8baef
MadhushaniRajakaruna <madhushani.hrajakaruna@gmail.com>
2022-10-18 15:16:34 +0530

    created adminDashboard

--- ES_project2/ES_project2/Form2.cs
+++ ES_project2/ES_project2/Form2.cs
@@ -21,5 +21,50 @@ namespace ES_project2
         {
             this.Close();
         }
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuImageButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuGradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bunifuMetroTextbox3_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
